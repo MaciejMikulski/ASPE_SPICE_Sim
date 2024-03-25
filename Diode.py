46b74d4cd5681a9ba58955fc525d2e70288335dd
MaciejMikulski <maciej.mikulski12@gmail.com>
2024-03-25 17:14:00 +0100

    Added diode parameters calculation

--- Diode.py
+++ Diode.py
@@ -1,19 +1,28 @@
 from Component import *
-
+import Constants
+import math
 
 class Diode(Component):
 
-    def __init__(self, id, ports, satCurr, emissionCoeff):
+    def __init__(self, id, ports, satCurr, emissionCoeff, temp=300.0):
         self._id = id
         self._type: ComponentType = ComponentType.DIO.name
         self._ports = []
         for x in ports:
             self.add_port(x)
         self._Is = satCurr
-        self._alpha = 1.0 / (emissionCoeff * 0.025)
+        fiT = (Constants.k * temp) / Constants.q
+        self._alpha = 1.0 / (emissionCoeff * fiT)
 
     def get_ports(self):
         return super().get_ports()
+
+    def getParams(self, Ud):
+        # TODO: add linearisation of the diode characteristics for extreme polarisations
+        Gd = self._alpha * self._Is * math.exp(self._alpha * Ud)
+        Id = self._Is * (math.exp(self._alpha * Ud) - 1.0)
+        Ieq = Id - Gd * Ud
+        return Gd, Ieq
 
     def print(self):
         print("Type: " + str(self._type) + str(self._id))
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from Circuit import *
 from Resistor import *
 from CurrentSource import *
 from VoltageSource import *
+from Diode import *
 
-R1 = Resistor(1, [1,2], 1000.0)
-R2 = Resistor(2, [2,3], 2000.0)
-R3 = Resistor(3, [2,3], 3000.0)
-IDD1 = CurrentSource(1, [3,1], 0.01)
-VDD1 = VoltageSource(1, [2,3], 5)
+R1 = Resistor(1, [1, 2], 1000.0)
+R2 = Resistor(2, [2, 3], 2000.0)
+R3 = Resistor(3, [2, 3], 3000.0)
+IDD1 = CurrentSource(1, [3, 1], 0.01)
+VDD1 = VoltageSource(1, [2, 3], 5)
+D1 = Diode(1, [1, 2], satCurr=10**(-12), emissionCoeff=1.0)
 
 circ = Circuit()
 circ.add_element(R1)
@@ -21,3 +23,5 @@ circ.add_element(VDD1)
 circ.set_gnd_node(3)
 circ.construct_matrix()
 print(circ.op_analisys())
+
+
