52e2506d2a15fd7394b880a96abe583d5546d574
MaciejMikulski <maciej.mikulski12@gmail.com>
2024-03-26 14:11:31 +0100

    Added simplified Newton-Rhapson solving

--- Circuit.py
+++ Circuit.py
@@ -6,10 +6,11 @@ class Circuit:
     def __init__(self):
         self._elements = []
         self._nodes = []
+        self._resultVect = np.empty(0)
         self._rightSideVect = np.empty(0)
         self._conductanceMatrix = np.empty(0)
         self._gndNode = 0
-        print(self._conductanceMatrix.size)
+        self._firsIteration = True
 
     def add_element(self, element: Component):
         self._elements.append(element)
@@ -18,10 +19,13 @@ class Circuit:
             if port not in self._nodes:
                 self._nodes.append(port)
         
-    def construct_matrix(self, prevVoltVect):
+    def construct_matrix(self):
         node_num = len(self._nodes)
         self._conductanceMatrix = np.zeros((node_num, node_num))
         self._rightSideVect = np.zeros((node_num, 1))
+        if self._firsIteration:
+            self._resultVect = np.zeros((node_num, 1))
+            self._firsIteration = False
 
         for component in self._elements:
             comp_type = component.get_type()
@@ -62,12 +66,14 @@ class Circuit:
                 self._conductanceMatrix = np.append(self._conductanceMatrix, tmpCol, axis=1)
                 self._conductanceMatrix = np.append(self._conductanceMatrix, tmpRow, axis=0)
                 self._rightSideVect = np.append(self._rightSideVect, tmpEl, axis=0)
+                tmpEl[0, 0] = 0.0
+                self._resultVect = np.append(self._resultVect, tmpEl, axis=0)
 
-            elif comp_type == ComponentType.VDD.name:
+            elif comp_type == ComponentType.DIO.name:
                 dioPorts = component.get_ports()
                 aNodeId = self._nodes.index(dioPorts[0])
                 kNodeId = self._nodes.index(dioPorts[1])
-                Ud = prevVoltVect[aNodeId] - prevVoltVect[kNodeId]
+                Ud = self._resultVect[aNodeId] - self._resultVect[kNodeId]
 
                 Gd, Ieq, Id = component.get_params(Ud)
 
@@ -96,9 +102,37 @@ class Circuit:
             raise Exception("Specified nonexistent node as GND.")
 
     def op_analisys(self):
-        return np.linalg.solve(self._conductanceMatrix, self._rightSideVect)
+        self.construct_matrix() # Dummy matrix construction to get final length of voltage vect
+
+
+        for i in range(0, 20):
+            _prevVoltageVect = self._resultVect
+
+            self.construct_matrix()
+
+            self._resultVect = np.linalg.solve(self._conductanceMatrix, self._rightSideVect)
+            self._resultVect = self._append_gnd_node(self._resultVect)
+            diff = np.absolute(np.subtract(_prevVoltageVect, self._resultVect))
+            if np.amax(diff) < 0.001:
+                print("Ended in " + str(i) + " iteration")
+                break
+
+        return self._resultVect
 
     def tran_analisys(self):
         pass
 
-    
+    def _append_gnd_node(self, voltVect):
+        rowNum = len(self._resultVect) + 1 # Add one node - GND
+        gndVoltVect = _voltageVectGND = np.zeros((rowNum, 1))
+        gndIndex = self._nodes.index(self._gndNode)
+
+        for i in range(0, rowNum):
+            if i < gndIndex:
+                gndVoltVect[i, 0] = voltVect[i, 0]
+            elif i == gndIndex:
+                gndVoltVect[i, 0] = 0.0
+            else:
+                gndVoltVect[i, 0] = voltVect[i-1, 0]
+
+        return gndVoltVect
