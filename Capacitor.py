34a3027e7b0d60227d516791481bc8a55f1db68c
MaciejMikulski <maciej.mikulski12@gmail.com>
2024-06-08 23:22:12 +0200

    Added parameter calculation method for capacitor

--- Capacitor.py
+++ Capacitor.py
@@ -1,6 +1,10 @@
 from Component import *
 import numpy as np
 
+class IntegratingRule(Enum):
+    INERP = 1
+    TRAPEZ = 2
+
 class Capacitor(Component):
 
     def __init__(self, id, ports, capacitance):
@@ -24,6 +28,35 @@ class Capacitor(Component):
     
     def get_ports(self):
         return super().get_ports()
+
+    def get_params(self, ud, id, stepLen, method=IntegratingRule.INERP):
+        """
+        This method calculates companion model parametes for capacitor using given
+        integrating method.
+
+        Parameters
+        ----------
+        ud : float
+            Voltage drop between positive and negative terminal of the capacitor in previous step.
+        id : float
+            Current flowing through capacitor in previous step.
+        stepLen : float
+            Length of simulation timestep in seconds.
+        method : constant
+            Type of integrating method represented by IntegratingRule enum.
+        Returns
+        Gc - conductance, Ieq - current of model's internal current cource.
+        ----------
+        """
+        Gc = 0.0
+        Ieq = 0.0
+        if method == IntegratingRule.INERP:
+            Gc = self._capacitance / stepLen
+            Ieq = -Gc * ud
+        elif method == IntegratingRule.TRAPEZ:
+            Gc = 2.0 * self._capacitance / stepLen
+            Ieq = -(-Gc * ud + id)
+        return Gc, Ieq
 
     def print(self):
         print("Type: " + str(self._type) + str(self._id))
